6b2e61e13d1ec5e2d0bee1ab911478c6f1e19c57
Amit <amitkumar0198singh@gmail.com>
2024-07-16 14:12:12 +0530

    sum and diff

---
+++ subtraction.py
@@ -0,0 +1,5 @@
+def substract_two_number(a, b):
+    return a - b
+
+
+print(substract_two_number(8, 5))
---
+++ sumation.py
@@ -0,0 +1,5 @@
+def add_two_number(a, b):
+    return a + b
+
+
+print(add_two_number(4, 5))
